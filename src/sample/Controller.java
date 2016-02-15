58f2262c1371a0c42d19db2ad54125e6ffa56b75
Josh <joshkreitz@gmail.com>
2016-02-15 00:25:43 -0600

    Began to figure this out

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    public void addMovie(){
+        Stage popout = new Stage();
+        popout.initModality(Modality.APPLICATION_MODAL);
+        popout.setTitle("Add Movie");
+
+        VBox layout = new VBox();
+
+        Scene scene = new Scene(layout, 250,250);
+        popout.setScene(scene);
+        popout.showAndWait();
+    }
+
 }
