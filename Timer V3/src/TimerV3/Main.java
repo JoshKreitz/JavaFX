c2571f82665127280cd437b8d3a0d3d76ae8ef86
Kalashnikove <KeepItBlack!!!1>
2016-04-25 11:00:21 -0500

    Worked on Timer V3

---
+++ Timer V3/src/TimerV3/Main.java
@@ -0,0 +1,31 @@
+package TimerV3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+//Main runner class. Starts off the program, that's it
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        {
+            //connects the MainWindow fxml file
+            Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+            window.setTitle("DMV Waitlist");
+
+            //stops the user from resizing the window, I didn't want to deal with image resizing for this project
+            //window.setResizable(false);
+
+            //window.setScene(new Scene(root, 800, 600));
+            window.setScene(new Scene(root, 1000, 600));
+            window.show();
+        }
+    }
+}
