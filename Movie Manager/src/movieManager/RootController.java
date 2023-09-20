239f611b090f820192009949e02af839ef4dc6bc
Josh Kreitz <jkreitz@umich.edu>
2023-09-19 20:33:12 -0600

    Stub out the config/settings page

--- Movie Manager/src/movieManager/RootController.java
+++ Movie Manager/src/movieManager/RootController.java
@@ -4,6 +4,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import movieManager.config.ConfigController;
 import movieManager.fileManager.FileManagerController;
 import movieManager.movieShelf.ShelfController;
 
@@ -16,23 +17,26 @@ public class RootController {
 	@FXML private Tab movieShelfTab;
 	@FXML private ShelfController movieShelfController;
 
-	public void init() {
-		tabPane.getSelectionModel().selectedItemProperty()
-				.addListener((ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) -> {
-					if (newValue == fileManagerTab) {
-						System.out.println("File Manager Tab");
-						System.out.println("fileManagerController=" + fileManagerController);
+	@FXML private Tab configTab;
+	@FXML private ConfigController configController;
 
-						// xxx_tab2bar_xxxController.handleTab2ButtonBar();
-					} 
-					else if (newValue == movieShelfTab) {
-						System.out.println("Movie Shelf Tab");
-						System.out.println("movieShelfController=" + movieShelfController);
-					}
-					else {
-						System.out.println("SOMETHING GOOFED");
-					}
-				});
+	public void init() {
+//		tabPane.getSelectionModel().selectedItemProperty()
+//				.addListener((ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) -> {
+//					if (newValue == fileManagerTab) {
+//						System.out.println("File Manager Tab");
+//						System.out.println("fileManagerController=" + fileManagerController);
+//
+//						// xxx_tab2bar_xxxController.handleTab2ButtonBar();
+//					} 
+//					else if (newValue == movieShelfTab) {
+//						System.out.println("Movie Shelf Tab");
+//						System.out.println("movieShelfController=" + movieShelfController);
+//					}
+//					else {
+//						System.out.println("SOMETHING GOOFED");
+//					}
+//				});
 	}
 
 }
---
+++ Movie Manager/src/movieManager/config/ConfigController.java
@@ -0,0 +1,16 @@
+package movieManager.config;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ConfigController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
